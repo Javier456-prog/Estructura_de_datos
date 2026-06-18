6800f426a4bd000afe723daf04dc6e229596b83f
Javier456-prog <rober.cabrera@unl.edu.ec>
2026-06-17 22:17:29 -0500

    Primer commit

---
+++ 00javascripts/juego.js
@@ -0,0 +1,21 @@
+const numeroSecreto = Math.floor(Math.random() * 10 + 1);
+
+const numeroJugador = parseInt(
+    prompt("Adivina el numero secreto entre el 1 al 10")
+);
+
+console.log(`Este es el numero con el que juegas ${numeroJugador}`);
+
+if (numeroJugador === numeroSecreto) {
+
+    console.log("Felicitaciones, adivinaste el numero secreto!!");
+
+} else if (numeroJugador < numeroSecreto) {
+
+    console.log("numero menor!!! intenta nuevamente");
+
+} else {
+
+    console.log("Numero mayor / muy alto, intenta nuevamente");
+
+}
---
+++ 00javascripts/operadores-comparacion.js
@@ -0,0 +1,24 @@
+// =====================================================
+// Tarea del dia martes 17/06/2026
+// =====================================================
+
+
+let numero1 = 10;
+let numero2 = 5;
+
+// Suma
+let suma = numero1 + numero2;
+console.log("La suma es: " + suma);
+
+// Resta
+let resta = numero1 - numero2;
+console.log("La resta es: " + resta);
+
+// Multiplicación
+let multiplicacion = numero1 * numero2;
+console.log("La multiplicación es: " + multiplicacion);
+
+// División
+let division = numero1 / numero2;
+console.log("La división es: " + division);
+
